e92154b86321c5695704bf56999fa0a374e87f07
Smatyab <abhayambaliya2020@gmail.com>
2023-10-02 16:53:39 +0530

    added profile image

--- lib/ui/home/register_screen.dart
+++ lib/ui/home/register_screen.dart
@@ -2,11 +2,11 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:hophseeflutter/core/utils.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
 import '../../core/share_preference.dart';
+import '../../core/utils.dart';
 import '../../core/widget/custom_text_field.dart';
 import '../../core/widget/date_picker.dart';
 import '../../data/datasource/api_services.dart';
@@ -28,7 +28,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   TextEditingController passwordController = TextEditingController();
   ApiServiceImpl apiService = ApiServiceImpl(Dio());
   String selectedGender = 'Male';
-  late File imageFile;
+  File? imageFile; // Initialize imageFile as nullable
 
   DateTime _selectedDate = DateTime.now();
 
@@ -50,29 +50,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Center(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.2,
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        child: Image.asset(
-                          'assets/applogo2.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                ProfileImagePicker(),
                 const SizedBox(height: 20),
-                const Divider(),
-                ElevatedButton(
-                    onPressed: () {
-                      _getImageFromUser();
-                    },
-                    child: Text("Upload")),
-                const SizedBox(height: 5),
-                // Text fields for first name and last name
                 TextFieldDesign(
                   hintText: 'Full Name',
                   labelText: 'Full Name',
@@ -82,9 +61,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     color: Colors.grey,
                   ),
                 ),
-
                 const SizedBox(height: 5),
-
                 TextFieldDesign(
                   hintText: 'Email',
                   labelText: 'Email',
@@ -94,9 +71,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     color: Colors.grey,
                   ),
                 ),
-
                 const SizedBox(height: 5),
-
                 TextFieldDesign(
                   hintText: 'Mobile Number',
                   labelText: 'Mobile Number',
@@ -106,17 +81,11 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     color: Colors.grey,
                   ),
                 ),
-
                 const SizedBox(height: 5),
-
-                /*Text(_selectedDate == null //ternary expression to check if date is null
-                    ? 'No date was chosen!'
-                    : 'Picked Date: $_selectedDate'),*/
                 CustomDatePicker(onClick: () {
                   pickDateDialog();
                 }),
                 const SizedBox(height: 5),
-
                 TextFieldDesign(
                   hintText: 'Password',
                   labelText: 'Password',
@@ -127,14 +96,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     color: Colors.black87,
                   ),
                 ),
-
-                // Gender Dropdown
                 const Padding(
                   padding: EdgeInsets.all(5.0),
                   child: GenderDropdown(),
                 ),
-
-                // Registration Button
                 Center(
                   child: Padding(
                     padding: const EdgeInsets.all(18.0),
@@ -151,34 +116,25 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           var dateOfBirth =
                               DateFormat("dd-MM-yyyy").format(_selectedDate);
                           var user = User(
-                              userName: userName,
-                              emailId: email,
-                              phoneNo: mobile,
-                              password: password,
-                              gender: gender.substring(0, 1),
-                              dateOfBirth: dateOfBirth);
+                            userName: userName,
+                            emailId: email,
+                            phoneNo: mobile,
+                            password: password,
+                            gender: gender.substring(0, 1),
+                            dateOfBirth: dateOfBirth,
+                          );
                           print("user : ${user.toJson()}");
                           ApiServiceImpl(Dio())
-                              .registerUser(user, imageFile)
+                              .registerUser(user, imageFile!)
                               .then((value) {
-                            // Run extra code here
                             if (value.error == 0) {
-                              loginUser(ApiServiceImpl(Dio()), context, email,
-                                  password);
+                              // Successfully registered, perform login or other actions
                             } else {
                               showSnackbar(context, "Something went wrong");
                             }
                           }, onError: (error) {
                             print(error);
                           });
-
-                          /* Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const Mylogin(),
-                            ),
-                            (route) => false,
-                          );*/
                         },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.lightBlueAccent,
@@ -196,9 +152,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                   ),
                 ),
-
-                // const Divider(),
-
                 const SizedBox(height: 20),
               ],
             ),
@@ -208,23 +161,76 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
+  Widget ProfileImagePicker() {
+    return GestureDetector(
+      onTap: _getImageFromUser,
+      child: Container(
+        width: 120,
+        height: 120,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.grey[200],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Stack(
+            children: [
+              ClipOval(
+                child: imageFile != null
+                    ? Image.file(
+                        imageFile!,
+                        width: 120,
+                        height: 120,
+                        fit: BoxFit.cover,
+                      )
+                    : Image.asset(
+                        'assets/pimage.png',
+                        width: 120,
+                        height: 120,
+                        fit: BoxFit.cover,
+                      ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  width: 36,
+                  height: 36,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.camera_alt,
+                    size: 24,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   void pickDateDialog() {
     showDatePicker(
-            context: context,
-            initialDate: DateTime.now(),
-            //which date will display when user open the picker
-            firstDate: DateTime(1950),
-            //what will be the previous supported year in picker
-            lastDate: DateTime
-                .now()) //what will be the up to supported date in picker
-        .then((pickedDate) {
-      //then usually do the future job
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1950),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
       if (pickedDate == null) {
-        //if user tap cancel then this function will stop
         return;
       }
       setState(() {
-        //for rebuilding the ui
         _selectedDate = pickedDate;
       });
     });
@@ -233,12 +239,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Future<void> _getImageFromUser() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(
-        source: ImageSource
-            .gallery); // You can also use ImageSource.camera to take a new photo.
+      source: ImageSource.gallery,
+    );
 
     if (pickedFile != null) {
-      imageFile = File(pickedFile.path);
-      // Do something with the selected image file, like displaying it or uploading it to a server.
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
     } else {
       // User canceled the image selection.
     }
