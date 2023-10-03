54404550d588557ae870e313c7a66418f5b43db4
Smatyab <abhayambaliya2020@gmail.com>
2023-10-03 05:43:01 +0530

    DOCTOR HOME

--- lib/ui/doctorpannel/doctor_home_screen.dart
+++ lib/ui/doctorpannel/doctor_home_screen.dart
@@ -1,34 +1,40 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hophseeflutter/data/module/user_model.dart';
+import '../../core/constant.dart';
 import '../../core/widget/custome_app_bar.dart';
-import '../dashboard/dashboard.dart';
-import '../profile/profile_design.dart';
+import '../../data/datasource/api_services.dart';
+import '../../data/module/appo_model.dart';
 
 class DoctorHomeScreen extends StatefulWidget {
   static const route = '/doctor_screen';
+
+  AppoList? appoList;
 
   @override
   _DoctorHomeScreenState createState() => _DoctorHomeScreenState();
 }
 
 class _DoctorHomeScreenState extends State<DoctorHomeScreen> {
-  List<Patient> patients = [
-    Patient(
-        name: 'Abhay Ambaliya',
-        age: 20,
-        gender: 'Male',
-        profileImageUrl: 'assets/doctor.png'), // Provide the image URL for John
-    Patient(
-        name: 'Kaushik Variya',
-        age: 25,
-        gender: 'Male',
-        profileImageUrl: 'assets/pimage.png'), // Provide the image URL for Jane
-    Patient(
-        name: 'Mehul Variya',
-        age: 35,
-        gender: 'Male',
-        profileImageUrl:
-            'assets/patient.png'), // Provide the image URL for Michael
-  ];
+  ApiServiceImpl apiService = ApiServiceImpl(Dio());
+  User? user;
+  final StreamController<AppoList?> _controller = StreamController<AppoList?>();
+
+  Stream<AppoList?> get stream => _controller.stream;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (widget.appoList == null) {
+      ApiServiceImpl(Dio()).getAppoList().then((value) {
+        _controller.sink.add(value);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,53 +44,29 @@ class _DoctorHomeScreenState extends State<DoctorHomeScreen> {
         child: Column(
           children: [
             const CustomAppBar(),
-            Divider(),
+            const Divider(),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.only(top: 10),
-                child: ListView.builder(
-                  itemCount: patients.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation:
-                          5.0, // Add elevation to the cards for a material look
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 20.0), // Adjust margin
-                      child: ListTile(
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                              20.0), // Adjust the border radius as needed
-                          child: Image.asset(
-                            patients[index]
-                                .profileImageUrl, // Load the image from the asset
-                            fit: BoxFit.cover, // Adjust the image fit
-                            width: 40.0,
-                            height: 40.0,
-                          ),
-                        ),
-                        title: Text(
-                          patients[index].name,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold, // Bold text
-                          ),
-                        ),
-                        subtitle: Text(
-                          'Age: ${patients[index].age} | Gender: ${patients[index].gender}',
-                          style: TextStyle(
-                            fontStyle: FontStyle.italic, // Italic text
-                            color: Colors.grey, // Text color
-                          ),
-                        ),
-                        trailing: const Icon(
-                          Icons
-                              .arrow_forward, // Add a trailing icon for a more interactive feel
-                          color: Colors.blue, // Icon color
-                        ),
-                        onTap: () {
-                          // Add an onTap action when a patient is tapped
-                          _showPatientDetails(patients[index]);
-                        },
-                      ),
+                child: StreamBuilder<AppoList?>(
+                  stream: stream, // Access the custom stream
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          Container(
+                              width: double.infinity,
+                              height: 170.h,
+                              child: UserListCard(
+                                  name: "${user?.userName}",
+                                  gender: "${user?.gender}",
+                                  age: "${user?.dateOfBirth}",
+                                  imagePath: user?.imageUrl ?? "")),
+                        ],
+                      );
+                    }
+                    return const Center(
+                      child: Text("Please wait.."),
                     );
                   },
                 ),
@@ -95,46 +77,115 @@ class _DoctorHomeScreenState extends State<DoctorHomeScreen> {
       ),
     );
   }
+}
 
-  // Function to show patient details when tapped
-  void _showPatientDetails(Patient patient) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Patient Details'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Name: ${patient.name}'),
-              Text('Age: ${patient.age}'),
-              Text('Gender: ${patient.gender}'),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Close'),
-            ),
-          ],
+class Patient extends StatelessWidget {
+  List<User> data;
+
+  Patient({required this.data, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      padding: const EdgeInsets.all(0),
+      itemCount: data.length,
+      itemBuilder: (BuildContext context, int index) {
+        User user = data[index];
+        return UserListCard(
+          name: "${user.userName}",
+          gender: "${user.gender}",
+          age: "${user.dateOfBirth}",
+          imagePath: user.imageUrl ?? "",
         );
       },
     );
   }
 }
 
-class Patient {
-  final String name;
-  final int age;
-  final String gender;
-  final String profileImageUrl;
+class UserListCard extends StatelessWidget {
+  const UserListCard({
+    Key? key,
+    required this.name,
+    required this.gender,
+    required this.age,
+    required this.imagePath,
+  }) : super(key: key);
 
-  Patient(
-      {required this.name,
-      required this.age,
-      required this.gender,
-      required this.profileImageUrl});
+  final String name;
+  final String gender;
+  final String imagePath;
+  final String age;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 150,
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              width: 90,
+              height: 90,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                shape: BoxShape.rectangle,
+                image: DecorationImage(
+                  image: NetworkImage("$host/$imagePath"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      gender,
+                      style: const TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                    Text(
+                      age,
+                      style: const TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
